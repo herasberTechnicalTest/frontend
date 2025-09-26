880e1459771458adc8f2fbd32c68f1a46e46307f
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-26 00:01:23 -0500

    feat(professionals/detail): add ProfessionalDetail view

---
+++ src/app/professional/features/detail/professional-detail/professional-detail.ts
@@ -0,0 +1,27 @@
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
+
+import {ProfessionalService} from '../../../core/services/professional_service';
+import {ProfessionalDTO} from '../../../shared/professionalDTO';
+import {PhotoGallery} from '../../../../shared/components/photo-gallery/photo-gallery';
+import {MapComponent} from '../../../../shared/components/map/map';
+
+@Component({
+  selector: 'app-professional-detail',
+  standalone: true,
+  imports:[NgIf, PhotoGallery, MapComponent],
+  templateUrl: './professional-detail.html',
+  styleUrl: './professional-detail.css'
+})
+export class ProfessionalDetail {
+  private route = inject(ActivatedRoute);
+  private api = inject(ProfessionalService);
+
+  pro = signal<ProfessionalDTO | null>(null);
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.api.getById(id).subscribe(p => this.pro.set(p));
+  }
+}
