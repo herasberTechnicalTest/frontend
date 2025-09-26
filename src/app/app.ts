ed79bb85d1a9272e4f99f11db7e0d2435500bb33
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-26 08:43:09 -0500

    feat(navbar): add navbar feature and fix routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:[RouterOutlet, RouterLink],
+  imports:[RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
