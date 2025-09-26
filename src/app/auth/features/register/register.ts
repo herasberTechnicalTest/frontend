8f24ca7ae8c839eb0d7a6b1bf9ab8b2b6c7c7e95
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-26 00:05:38 -0500

    feat(auth/register): create RegisterComponent (standalone)

---
+++ src/app/auth/features/register/register.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import {ProfessionalService} from '../../../professional/core/services/professional-service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports:[FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class RegisterComponent {
+  private api = inject(ProfessionalService);
+  private router = inject(Router);
+
+  model: any = {
+    fullName: '', email: '', password: '',
+    phone: '', servicesDescription: '',
+    photoUrl: '', gallery: [],
+    rate: 0, currency: 'PEN',
+    countryName: 'Perú', cityName: '', districtName: '',
+    mapsUrl: '', whatsappLink: '' // opcional
+  };
+
+  submit(){
+    this.api.create(this.model).subscribe({
+      next: () => this.router.navigateByUrl('/login'),
+      error: e => alert(e.error?.message || 'Error al registrar')
+    });
+  }
+}
