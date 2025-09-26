f4eb03e74b6958c6ff3212e71bf404f5d197938a
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-25 23:53:34 -0500

    refactor(routes): wire routes for auth and professionals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './auth/features/login/login';
+import {RegisterComponent} from './auth/features/register/register';
+import {ProfessionalsList} from './professional/features/list/professionals-list/professionals-list';
+import {ProfessionalDetail} from './professional/features/detail/professional-detail/professional-detail';
+import {authGuard} from './auth/core/guards/auth_guard';
+import {ProfileEditor} from './professional/features/profile-editor/profile-editor/profile-editor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'pros', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: 'pros', component: ProfessionalsList, title: 'Profesionales' },
+  { path: 'pros/:id', component: ProfessionalDetail, title: 'Perfil' },
+  { path: 'me/profile', component: ProfileEditor, canActivate: [authGuard], title: 'Mi perfil' },
+  { path: '**', redirectTo: 'pros' }
+];
