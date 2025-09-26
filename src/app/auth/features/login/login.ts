97dace5f69f9cc8e60b219650178f2a90b351bdd
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-25 23:55:47 -0500

    feat(auth/login): create LoginComponent (standalone)

---
+++ src/app/auth/features/login/login.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import {AuthService} from '../../core/services/auth_service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports:[FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class LoginComponent {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+  email=''; password='';
+
+  submit(){
+    this.auth.login(this.email, this.password).subscribe({
+      next: r => { this.auth.setToken(r.token); this.router.navigateByUrl('/me/profile'); },
+      error: e => alert(e.error?.message || 'Error de autenticación')
+    });
+  }
+}
