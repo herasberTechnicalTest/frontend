1176506acadc6fb6cbb3f014c677c6eb6f611ad9
Rafooo6 <rafaelbravo032@gmail.com>
2025-09-26 00:09:39 -0500

    feat(bootstrap): fix some libraries

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {tokenInterceptor} from './app/auth/core/interceptors/token_interceptor';
+import {tokenInterceptor} from './app/auth/core/interceptors/token-interceptor';
 import {provideRouter} from '@angular/router';
 import {routes} from './app/app.routes';
 
